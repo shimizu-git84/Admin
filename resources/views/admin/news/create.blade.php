57fcee0614ac44f3405397eed0cc8ac49a09ac52
shimizu <dkdkznzn9@gmail.com>
2020-08-25 13:14:17 +0000

    commit

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,12 +1,16 @@
-<!DOCTIPE>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="XUA-compatible"content="IE=edge">
-        <meta name="viewport" content="width-device-width,inital-scale=1">
-        <title>mynews</title>
-    </head>
-    <body>
-        <h1>myニュース作成画面</h1>
-    </body>
-</html>
+{{-- layouts/admin.blade.phpを読みこむ --}}
+@extends('layouts.admin')
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'ニュースの新規作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="contaier">
+        <div class ="row">
+            <div class="col-md-8 mx-auto">
+                <h2>ニュース新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
